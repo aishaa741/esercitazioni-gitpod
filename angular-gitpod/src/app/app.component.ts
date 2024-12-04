ced8ac0ad360d5e348216e4948360be66848103d
aishaa741 uyyu <mahmoud.aisha@iisgalvanimi.edu.it>
2024-12-04 13:28:10 +0000

    prova

--- angular-gitpod/src/app/app.component.ts
+++ angular-gitpod/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondoComponent } from './secondo/secondo.component';
+import { TerzoComponent } from './terzo/terzo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SecondoComponent,TerzoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
